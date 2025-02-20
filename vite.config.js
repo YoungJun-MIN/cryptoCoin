ce7169b1af3141b0e0aa656c747fe4ca5ae879a7
YJ Min <mesutjune@gmail.com>
2025-02-20 14:02:30 -0600

    Initial project setup:

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // @를 src 폴더로 설정
+      '@assets': path.resolve(__dirname, 'src/assets'), // @assets를 src/assets 폴더로 설정
+      '@components': path.resolve(__dirname, 'src/components') // @components를 src/components 폴더로 설정
+    }
+  }
 })
