7eb49e95372cdc1912abc1681870145634e3f0dc
YJ Min <mesutjune@gmail.com>
2025-02-25 21:26:33 -0600

    modify tick timeFormat tick

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react"
 import { useSelector } from "react-redux"
-import { select, scaleTime, timeParse, extent, scaleLinear, min, max, line, area, curveLinear, axisBottom, axisLeft } from "d3";
+import { select, scaleTime, timeParse, extent, scaleLinear, min, max, line, area, curveLinear, axisBottom, axisLeft, timeFormat } from "d3";
 import priceData from "@api/price.json"
 import useResizeObserver from "@hooks/useResizeObserver"
 import styles from '@components/Chart/Chart.module.css';
@@ -14,7 +14,7 @@ export default function Chart() {
     const svg = select(svgRef.current);
     const svgContent = svg.select(".chart__content");
     const { width, height } = dimensions || wrapperRef.current.getBoundingClientRect();
-    const margin = { top: 40, right: 40, bottom: 40, left: 40};
+    const margin = { top: 40, right: 40, bottom: 40, left: 50};
     const innerWidth = width - margin.right - margin.left;
     const innerHeight = height - margin.top - margin.bottom;
     const xScale = scaleTime()
@@ -28,10 +28,10 @@ export default function Chart() {
     const yScale = scaleLinear()
       .domain([min(priceData, (item) => {
           const [, price] = item;
-          return price;
+          return price * 0.99;
         }), max(priceData, (item) => {
           const [,price] = item;
-          return price;
+          return price * 1.01;
         })
       ])
       .range([innerHeight - 0, 0]);
@@ -59,21 +59,30 @@ export default function Chart() {
     })
     .curve(curveLinear);
     svg
+      .attr("width", width - margin.left)
+      .attr("height", height - margin.top)
       .attr("transform", `translate(${margin.left}, 0)`)
 
     // Axes
-    const xAxis = axisBottom(xScale);
+    const xAxis = axisBottom(xScale)
+      .tickSizeOuter(0)
+      .tickFormat(timeFormat("%H:%M"));
     svg
       .select(".x-axis")
       .attr("transform", `translate(0, ${innerHeight})`)
       .call(xAxis);
 
-    const yAxis = axisLeft(yScale);
+    const yAxis = axisLeft(yScale).ticks(5).tickSize(-innerWidth).tickPadding(10);
+    // const yAxis = axisLeft(yScale).tickValues(yScale.ticks(5));
     svg.select(".y-axis")
     .call(yAxis)
-    // .attr("transform", `translate(0, 0)`)
-    .selectAll(".domain, .tick line") // y축 선과 눈금선 동시에 선택
+    // .selectAll(".domain, .tick line") // y축 선과 눈금선 동시에 선택
+    .selectAll(".domain") // y축선 제거
     .remove()
+
+    svg.selectAll(".y-axis .tick line")
+      .attr("stroke", "#ccc") // 그리드 선 색상 (연한 회색)
+      .attr("stroke-dasharray", "3,3"); // 점선 스타일 (선택 사항)
 
     svgContent
     .selectAll(".myArea")
@@ -100,15 +109,12 @@ export default function Chart() {
       <div id="chart__wrapper" className={`${styles.chartWrapper}`} ref={wrapperRef}>
         <svg ref={svgRef} className={`${styles.svg}`}>
           <defs>
-            <clipPath id="chart-clip">
-              <rect x="0" y="0" width={innerWidth} height={innerHeight}></rect>
-            </clipPath>
             <linearGradient id="gradienta" x1="0%" y1="0%" x2="0%" y2="100%">
               <stop offset="0%" style={{ stopColor: "#84CBFF", stopOpacity: 0.5 }} />
               <stop offset="100%" style={{ stopColor: "#DCDCDC", stopOpacity: 0 }} />
             </linearGradient>
           </defs>
-          <g className={`chart__content ${styles.chartContent}`} clipPath="url(#chart-clip)"></g>
+          <g className={`chart__content`}></g>
           <g className="x-axis" />
           <g className="y-axis" />
         </svg>
